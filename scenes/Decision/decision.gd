dc2931ede4ee7ab2972bdd60f0a6f738a03b0875
neiotic <javialeborre@gmail.com>
2026-07-25 16:52:07 +0200

    ajustada decision

--- scenes/Decision/decision.gd
+++ scenes/Decision/decision.gd
@@ -3,11 +3,20 @@ extends Control
 const OPTION_SCENE = preload("res://scenes/opcion/opcion.tscn") #Precargo la escena opcion
 @onready var container: HBoxContainer = $HBoxContainer #container va a ser el hbox
 @export var option_keys : Array[String]
+var active_option : int = -1
 
-func _ready() :
+signal option_selected_effects(effect: Dictionary) #Declaro la señal de que se emitirá cuando pulse el boton, y que emite un diccionario
+
+func on_option_press(option : Option):
+	option_selected_effects.emit(option.data.effect) #Que es emitir lo que tiene en su campo de efecto
+
+func _ready():
 	for esta_optionkey in option_keys : #Recorro el array (se lo he pasado por el editor)
 		#print(esta_optionkey) ESTO ES PARA PRUEBAS
-		var option = OPTION_SCENE.instantiate() #Me creo una opcion
+		var option: Option = OPTION_SCENE.instantiate() #Me creo una instancia de opcion
 		option.option_key = esta_optionkey #Le paso que su optionkey será la de esta iteración de array
-		container.add_child(option) #Se la añado como hijo al hbox
+
+		option.option_selected.connect(on_option_press)
+		
+		container.add_child(option) #añado la opción como hijo al hbox
 		
--- scenes/opcion/opcion.gd
+++ scenes/opcion/opcion.gd
@@ -1,6 +1,8 @@
 extends Button
+class_name Option
 @onready var title: Label = $MarginContainer/VBoxContainer/Title
 @onready var cost: Label = $MarginContainer/VBoxContainer/Cost
+signal option_selected(option: Option)
 
 
 @export var option_key: String #optionkey es una variable que se pasa por el editor
@@ -9,7 +11,8 @@ var data : Dictionary
 
 func _ready():
 	data = DataResources.options_data_map[option_key] #Los datos serán los que saque del mapa, que coincidan con su option_key
-	print ("Datos:")
-	print(data)
 	title.text = data.title #El título será el que ela de los datos
-	cost.text = str(data.cost) + " @s" #El coste ídem
+	cost.text = "%d @" % data.cost #El coste ídem
+
+func _on_pressed() -> void:
+	option_selected.emit(self)
