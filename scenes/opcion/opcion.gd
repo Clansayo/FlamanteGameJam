a254046b6636b58f2e6ac33fecc5c282ecdd60e5
Clansayo <133772950+Clansayo@users.noreply.github.com>
2026-07-25 23:50:47 +0200

    todo bien creo ayuda

--- scenes/opcion/opcion.gd
+++ scenes/opcion/opcion.gd
@@ -20,7 +20,7 @@ func _ready():
 
 func _on_pressed() -> void:
 	option_selected.emit(self)
-	# AudioManager.play_ui_click()
+	AudioManager.play_ui_click()
 
 func activate_option():
 	self.modulate = Color(0.716, 0.0, 0.422, 1.0)
