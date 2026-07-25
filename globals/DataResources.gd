73e86f4e5e2ea6c65a30bbe0387bca6a82dc2ba1
Clansayo <133772950+Clansayo@users.noreply.github.com>
2026-07-25 13:41:10 +0200

    added dataResources singleton to load resources

--- globals/DataResources.gd
+++ globals/DataResources.gd
@@ -1,1 +1,11 @@
 extends Node
+## Esta clase es un singleton global que contiene los archivos de datos json cargados
+## a diccionarios.
+
+const DATA_PATH: String = "gameData/"
+
+var options_data_map: Dictionary
+
+func _ready() -> void:
+	print("**** LOADING GAME DATA FILES ****")
+	options_data_map = FileManager.load_json(DATA_PATH + "opciones.json")
