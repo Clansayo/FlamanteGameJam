f1b5672cedca6522d501ed735affedb0853fd0bd
Clansayo <133772950+Clansayo@users.noreply.github.com>
2026-07-26 01:51:27 +0200

    OPCIONES OTRA VEZ PROGRAMADAS SO BACK

--- scenes/opcion/opcion.gd
+++ scenes/opcion/opcion.gd
@@ -1,7 +1,6 @@
 extends TextureButton
 class_name Option
 
-
 @onready var cost: Label = $MarginContainer/Cost
 @onready var title_label: RichTextLabel = $TitleLabel
 
@@ -14,13 +13,55 @@ signal option_selected(option: Option)
 var data : Dictionary 
 
 func _ready():
+	effect_description_label.visible = false
 	data = DataResources.options_data_map[option_key] #Los datos serán los que saque del mapa, que coincidan con su option_key
 	title_label.text = data.title #El título será el que ela de los datos
 	cost.text = "%d @" % data.cost #El coste ídem
-	effect_description_label.visible = false
+	
+	effect_description_label.text = ""
+	
+	var effect: Dictionary = data.effect
+	var points: int = 0
+	const UMBRAL_MUY_MALO = 20
+	if effect.has("add_dollars"):
+		points = effect.add_dollars
+		if points > 0:
+			effect_description_label.text += "+%s M$\n" % effect.add_dollars
+		else:
+			effect_description_label.text += "%s M$\n" % effect.add_dollars
+	if effect.has("add_email_points"):
+		points = effect.add_email_points
+		if points > 0:
+			effect_description_label.text += "+%s @\n" % effect.add_dollars
+		else:
+			effect_description_label.text += "%s @\n" % effect.add_dollars
+	if effect.has("add_earth_points"):
+		points = effect.add_earth_points
+		if points > UMBRAL_MUY_MALO:
+			effect_description_label.text += "TERRIBLE para el medio ambiente\n"
+		elif points > 0:
+			effect_description_label.text += "Malillo para el medio ambiente\n"
+		else:
+			effect_description_label.text += "Bueno para el medio ambiente\n"
+	if effect.has("add_boss_points"):
+		points = effect.add_boss_points
+		if points > UMBRAL_MUY_MALO:
+			effect_description_label.text += "Al jefe no le mola nada esto\n"
+		elif points > 0:
+			effect_description_label.text += "Malillo para el medio ambiente\n"
+		else:
+			effect_description_label.text += "Bueno para el medio ambiente\n"
+	
+	if effect.has("add_dollars_modifier"):
+		points = effect.add_dollars_modifier
+		if points > 0:
+			effect_description_label.text += "+%s M$/segundo\n" % effect.add_dollars
+		else:
+			effect_description_label.text += "%s M$/segundo\n" % effect.add_dollars
 
 func _on_pressed() -> void:
 	option_selected.emit(self)
+	# AudioManager.play_ui_click()
 
 func activate_option():
 	self.modulate = Color(0.716, 0.0, 0.422, 1.0)
