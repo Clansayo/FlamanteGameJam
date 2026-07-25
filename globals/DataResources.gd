6348389d2eccda5a72b0eef402a42227ca058572
Clansayo <133772950+Clansayo@users.noreply.github.com>
2026-07-25 23:50:08 +0200

    hola

--- globals/DataResources.gd
+++ globals/DataResources.gd
@@ -5,7 +5,9 @@ extends Node
 const DATA_PATH: String = "gameData/"
 
 var options_data_map: Dictionary
+var notifications_data_map: Dictionary
 
 func _ready() -> void:
 	print("**** LOADING GAME DATA FILES ****")
 	options_data_map = FileManager.load_json(DATA_PATH + "opciones.json")
+	notifications_data_map = FileManager.load_json(DATA_PATH + "notifications.json")
--- scenes/notificacion/notification.gd
+++ scenes/notificacion/notification.gd
@@ -1,1 +1,9 @@
 extends PanelContainer
+
+var data: Dictionary
+
+@export var notification_key: String
+
+func _ready() -> void:
+	data = DataResources.notifications_data_map[notification_key]
+	print(data)
