ea848d55021547d4e5fba03e4f218fc6f0ae223e
Clansayo <133772950+Clansayo@users.noreply.github.com>
2026-07-25 21:15:04 +0200

    Version dos opciones

--- scenes/opcion/opcion.gd
+++ scenes/opcion/opcion.gd
@@ -5,15 +5,15 @@ class_name Option
 @onready var cost: Label = $MarginContainer/VBoxContainer/Cost
 signal option_selected(option: Option)
 @onready var effect_description_timer: Timer = $effect_description_timer
-@onready var effects_description: PanelContainer = $EffectsDescription
-@onready var effect_description_label: RichTextLabel = $EffectsDescription/MarginContainer/EffectDescriptionLabel
+@onready var effect_description_label: RichTextLabel = $MarginContainer/EffectDescriptionLabel
+@onready var v_box_container: VBoxContainer = $MarginContainer/VBoxContainer
 
 @export var option_key: String #optionkey es una variable que se pasa por el editor
 
 var data : Dictionary 
 
 func _ready():
-	effects_description.visible = false
+	effect_description_label.visible = false
 	data = DataResources.options_data_map[option_key] #Los datos serán los que saque del mapa, que coincidan con su option_key
 	title.text = data.title #El título será el que ela de los datos
 	cost.text = "%d @" % data.cost #El coste ídem
@@ -26,9 +26,15 @@ func _ready():
 	if effect.has("add_dollars"):
 		points = effect.add_dollars
 		if points > 0:
-			effect_description_label.text += "+%s$\n" % effect.add_dollars
+			effect_description_label.text += "+%s M$\n" % effect.add_dollars
 		else:
-			effect_description_label.text += "%s$\n" % effect.add_dollars
+			effect_description_label.text += "%s M$\n" % effect.add_dollars
+	if effect.has("add_email_points"):
+		points = effect.add_email_points
+		if points > 0:
+			effect_description_label.text += "+%s @\n" % effect.add_dollars
+		else:
+			effect_description_label.text += "%s @\n" % effect.add_dollars
 	if effect.has("add_earth_points"):
 		points = effect.add_earth_points
 		if points > UMBRAL_MUY_MALO:
@@ -45,6 +51,13 @@ func _ready():
 			effect_description_label.text += "Malillo para el medio ambiente\n"
 		else:
 			effect_description_label.text += "Bueno para el medio ambiente\n"
+	
+	if effect.has("add_dollars_modifier"):
+		points = effect.add_dollars_modifier
+		if points > 0:
+			effect_description_label.text += "+%s M$/segundo\n" % effect.add_dollars
+		else:
+			effect_description_label.text += "%s M$/segundo\n" % effect.add_dollars
 
 func _on_pressed() -> void:
 	option_selected.emit(self)
@@ -56,11 +69,13 @@ func deactivate_option():
 	self.modulate = Color(1.0, 1.0, 1.0, 1.0)
 
 func _on_effect_description_timer_timeout() -> void:
-	effects_description.visible = true
+	effect_description_label.visible = true
+	v_box_container.visible = false
 
 func _on_mouse_entered() -> void:
 	effect_description_timer.start()
 
 func _on_mouse_exited() -> void:
 	effect_description_timer.stop()
-	effects_description.visible = false
+	effect_description_label.visible = false
+	v_box_container.visible = true
