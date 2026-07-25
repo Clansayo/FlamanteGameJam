421ca1d0cd65169bdd865c486f57c8424844db97
Clansayo <133772950+Clansayo@users.noreply.github.com>
2026-07-26 00:15:33 +0200

    Notificaciones template

--- scenes/notificacion/notification.gd
+++ scenes/notificacion/notification.gd
@@ -4,6 +4,16 @@ var data: Dictionary
 
 @export var notification_key: String
 
+@onready var title: Label = $MarginContainer/Title
+@onready var content_label: RichTextLabel = $MarginContainer/VBoxContainer/MarginContainer/ContentLabel
+@onready var image: TextureRect = $MarginContainer/VBoxContainer/Image
+
+const IMAGE_BASE_PATH = "res://resources/notification_images/"
+
 func _ready() -> void:
 	data = DataResources.notifications_data_map[notification_key]
-	print(data)
+	title.text = "Nota del Jefe" if data.type == "boss" else "Últimas Noticias"
+	content_label.text = data.message
+	if data.has("image"):
+		image.texture = load(IMAGE_BASE_PATH + data.image)
+	
