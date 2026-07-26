f407209ac6c5f1ac637dabca4c2ee080d3b173fe
katastraa <madeleine2144@gmail.com>
2026-07-26 13:42:32 +0200

    twens trans

--- scenes/Pantalla de inicio/pantalla_de_inicio.gd
+++ scenes/Pantalla de inicio/pantalla_de_inicio.gd
@@ -2,6 +2,8 @@ extends Control
 
 @onready var creditos: CanvasLayer = $Creditos
 @onready var canvas_layer: CanvasLayer = $CanvasLayer
+var start_size = 1
+var end_size = 1.04
 
 func _ready() -> void:
 	pass # Replace with function body.
@@ -22,3 +24,34 @@ func _on_salir_pressed() -> void:
 
 func _on_iniciar_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/Pantalla de inicio/Tiburon.tscn")
+
+
+func _on_salir_mouse_entered() -> void:
+	var tween: Tween = create_tween()
+	tween.tween_property($CanvasLayer/Salir,"scale", Vector2(end_size, end_size), 0.4)
+	tween.set_trans(Tween.TRANS_BOUNCE)
+	
+func _on_salir_mouse_exited() -> void:
+	var tween: Tween = create_tween()
+	tween.tween_property($CanvasLayer/Salir,"scale", Vector2(start_size, start_size), 0.4)
+	tween.set_trans(Tween.TRANS_BOUNCE)
+
+func _on_creditos_mouse_entered() -> void:
+	var tween: Tween = create_tween()
+	tween.tween_property($CanvasLayer/Creditos,"scale", Vector2(end_size, end_size), 0.4)
+	tween.set_trans(Tween.TRANS_BOUNCE)
+	
+func _on_creditos_mouse_exited() -> void:
+	var tween: Tween = create_tween()
+	tween.tween_property($CanvasLayer/Creditos,"scale", Vector2(start_size, start_size), 0.4)
+	tween.set_trans(Tween.TRANS_BOUNCE)
+	
+func _on_iniciar_mouse_entered() -> void:
+	var tween: Tween = create_tween()
+	tween.tween_property($CanvasLayer/Iniciar,"scale", Vector2(end_size, end_size), 0.4)
+	tween.set_trans(Tween.TRANS_BOUNCE)
+
+func _on_iniciar_mouse_exited() -> void:
+	var tween: Tween = create_tween()
+	tween.tween_property($CanvasLayer/Iniciar,"scale", Vector2(start_size, start_size), 0.4)
+	tween.set_trans(Tween.TRANS_BOUNCE)
