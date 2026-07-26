e575c82f36f243029bcb6d24a93e2b5fee4cf7e3
Clansayo <133772950+Clansayo@users.noreply.github.com>
2026-07-26 03:17:27 +0200

    cambio nombre imagenes + notificaciones

--- scenes/notificacion/notification.gd
+++ scenes/notificacion/notification.gd
@@ -8,12 +8,21 @@ var data: Dictionary
 @onready var content_label: RichTextLabel = $MarginContainer/VBoxContainer/MarginContainer/ContentLabel
 @onready var image: TextureRect = $MarginContainer/VBoxContainer/Image
 
-const IMAGE_BASE_PATH = "res://resources/notification_images/"
+const IMAGE_BASE_PATH = "res://resources/sprites/Notificaciones/"
+var path_tipo: String
 
 func _ready() -> void:
 	data = DataResources.notifications_data_map[notification_key]
-	title.text = "Nota del Jefe" if data.type == "boss" else "Últimas Noticias"
+	if data.type == "boss":
+		title.text = "Nota del Jefe"
+		path_tipo = "Jefe/"
+	else:
+		title.text = "Últimas Noticias"
+		path_tipo = "Naturaleza/"
+
 	content_label.text = data.message
+
 	if data.has("image"):
-		image.texture = load(IMAGE_BASE_PATH + data.image)
+		print("Path: %s" % IMAGE_BASE_PATH + path_tipo + data.image)
+		image.texture = load(IMAGE_BASE_PATH + path_tipo + data.image)
 	
