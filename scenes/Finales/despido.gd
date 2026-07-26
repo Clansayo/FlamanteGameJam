0e2ccefb9470a58b0bb0d22c8dfb9f9a09e9d528
katastraa <madeleine2144@gmail.com>
2026-07-26 13:17:31 +0200

    botones final

--- scenes/Finales/despido.gd
+++ scenes/Finales/despido.gd
@@ -9,3 +9,9 @@ func _ready() -> void:
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("click"):
 		SignalBus.display_dialogue.emit(dialogue_area)
+
+
+
+
+func _on_texture_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/Pantalla de inicio/pantalla de inicio.tscn")
