6e366bdb3bac6b79606b55c52ceaf59344b7d83d
Clansayo <133772950+Clansayo@users.noreply.github.com>
2026-07-26 11:46:10 +0200

    sonidos dde nnotificaciones

--- scenes/notificacion/generador_notificaciones.gd
+++ scenes/notificacion/generador_notificaciones.gd
@@ -11,5 +11,7 @@ func generate(notificatinon_key: String):
 	new_notification.global_position = Vector2(randf_range(20, 1300), randf_range(0, 600))
 	print("moved to: %s" % new_notification.position)
 	
+	AudioManager.play_notification()
+	
 	new_notification.load_data()
 	DataResources.notifications_data_map.erase(notificatinon_key)
