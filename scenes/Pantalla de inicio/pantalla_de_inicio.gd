1d87bb9aea0e5c3ef605fa1182b71b57a00060ed
katastraa <madeleine2144@gmail.com>
2026-07-26 02:34:33 +0200

    se la llevo el tiburon

--- scenes/Pantalla de inicio/pantalla_de_inicio.gd
+++ scenes/Pantalla de inicio/pantalla_de_inicio.gd
@@ -21,4 +21,4 @@ func _on_salir_pressed() -> void:
 	get_tree().quit()
 
 func _on_iniciar_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/Main/MainScene.tscn")
+	get_tree().change_scene_to_file("res://scenes/Pantalla de inicio/Tiburon.tscn")
